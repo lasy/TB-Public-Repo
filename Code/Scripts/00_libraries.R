87d119f02147b92d4aff6c1c10221cd5ce0cc9a7
LaSy <laurasymul@gmail.comn>
2019-06-17 18:18:44 -0700

    Update 00_libraries.R

--- Code/Scripts/00_libraries.R
+++ Code/Scripts/00_libraries.R
@@ -26,7 +26,7 @@ for(package in package_list){
   cat(package,"\n")
   pckgs = installed.packages()
   need.to.install = (!(package %in% pckgs[,1]))
-  if(need.to.install){cat('installing package ',package,'\n');install.packages(package,dependencies = TRUE)}
+  if(need.to.install){cat('installing package ',package,'\n');install.packages(package,repos = "https://cloud.r-project.org",dependencies = TRUE)}
   library(package = package, character.only = TRUE)
 }
 
